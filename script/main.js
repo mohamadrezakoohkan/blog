e9878ab9bcc65ade574828bcff9ec685a3f09f15
Mohammadreza <mohamad_koohkan@icloud.com>
2020-10-15 00:58:45 +0330

    Initial commit

---
+++ script/main.js
@@ -0,0 +1,26 @@
+
+function selectUnderline(event) {
+    var parent = event.target.parentElement;
+    var underlines = getAllUnderlines()
+    var underline = getUnderline(parent);
+    removeId(underlines);
+    makeVisible(underline);
+}
+
+function getAllUnderlines() {
+    return document.getElementsByClassName("underline")
+}
+
+function getUnderline(parent) {
+    return parent.getElementsByClassName("underline")[0]
+}
+
+function makeVisible(element) {
+    element.id = "visible";
+}
+
+function removeId(elements) {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].id = "";
+    }
+}
